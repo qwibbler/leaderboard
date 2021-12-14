60186314d055194f759787abbd1410093a76b405
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-12-14 14:37:11 -0600

    Build with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  watch: true,
+  // watch: true,
   mode: 'development',
   entry: {
     index: './src/index.js',
