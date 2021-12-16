15fbb85b5368cf9f732f6295bf59b6d3a9cb0432
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-12-16 12:23:29 -0600

    Build and screenshot

--- src/url.js
+++ src/url.js
@@ -1,6 +1,6 @@
 const url = {
   test: 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/IcHXit5dDPeCw71PNKqn/scores',
-  pretty: 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/AjJ3JLNe5NvdQsm7WCSu/scores'
-}
+  pretty: 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/AjJ3JLNe5NvdQsm7WCSu/scores',
+};
 
-export default url.test;
+export default url.pretty;
